6851207d89819b1a282856b0915fa3fa0d900d7a
ThatDudeWhoIsBadAtCoding <90434651+ThatDudeWhoIsBadAtCoding@users.noreply.github.com>
2021-09-10 21:32:09 +0530

    Added a home screen, background and difficulties

--- game.py
+++ game.py
@@ -1,21 +1,63 @@
-import pygame
-import random
-import time
+import pygame, random, time
 
 run = True
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+image = pygame.image.load("background.png")
+picture = pygame.transform.scale(image, (800, 600))
+screen.blit(picture, (0, 0))
+pygame.display.update()
+
 bodyParts = 3
 space = 20
-speed = .05
-snake_color = (0, 255, 255)
+speed = .06
+snake_color = 209, 15, 206
 direction = "right"
 score = 0
 string = "Score : {}"
 font = pygame.font.Font('freesansbold.ttf', 32)
+small = pygame.font.Font('freesansbold.ttf', 16)
 pygame.display.set_caption('Snake Game')
-text = font.render(string.format(score), True, (255, 255, 255))
 disabled = False
+# creating the difficulty buttons
+title_screen1 = font.render("Welcome To...Snake!!", True, (255, 255, 255))
+screen.blit(title_screen1, (250, 150))
+cap = small.render("Choose difficulty!", True, (255, 255, 255))
+screen.blit(cap, (350, 200))
+e1 = small.render("easy", True, (0, 0, 0))
+m1 = small.render("medium", True, (0, 0, 0))
+h1 = small.render("hard", True, (0, 0, 0))
+
+easy = pygame.Rect(350, 300, 100, 50)
+pygame.draw.rect(screen, (14, 156, 23), easy)
+screen.blit(e1, (380, 315))
+med = pygame.Rect(350, 400, 100, 50)
+pygame.draw.rect(screen, (217, 245, 5), med)
+screen.blit(m1, (370, 415))
+hard = pygame.Rect(350, 500, 100, 50)
+pygame.draw.rect(screen, (201, 22, 22), hard)
+screen.blit(h1, (380, 515))
+choosing = True
+while choosing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if easy.collidepoint(mouse_pos):
+                speed = 0.06
+                choosing = False
+            if med.collidepoint(mouse_pos):
+                speed = 0.05
+                choosing = False
+            if hard.collidepoint(mouse_pos):
+                speed = 0.04
+                choosing = False
+    pygame.display.update()
+
+
+text = font.render(string.format(score), True, (255, 255, 255))
+
 
 def draw(ifFoodCollected):
     global snake
@@ -23,7 +65,8 @@ def draw(ifFoodCollected):
     global space
     global text
     global string
-    screen.fill((0, 0 ,0))
+    global picture
+    screen.blit(picture, (0, 0))
     snake.draw()
     text = font.render(string.format(score), True, (255, 255, 255))
     screen.blit(text, (300, 0))
@@ -35,9 +78,9 @@ def draw(ifFoodCollected):
 
 def collision(snok):
     x, y = snake.cords[0]
-    if x < 0 or x >= 800:
+    if x < 0 or x > 800:
         return True
-    if y < 0 or y >= 600:
+    if y < 0 or y > 600:
         return True
     for i in snake.cords[1:]:
         if x == i[0] and y == i[1]:
@@ -108,6 +151,7 @@ def changeDirection(newDir):
     if disabled:
         return
     disabled = True
+
     if newDir == "left":
         if direction != "right":
             direction = newDir
